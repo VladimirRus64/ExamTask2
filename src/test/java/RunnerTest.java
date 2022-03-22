d6eb95a02fdb75fa38a5c496df680d4f2f4d36a4
VladimirGarin <torntwik@gmail.com>
2022-03-22 22:05:26 +0400

    final ver.2

--- src/test/java/RunnerTest.java
+++ src/test/java/RunnerTest.java
@@ -1,18 +1,24 @@
-import hooks.WebHooks;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-import org.junit.jupiter.api.extension.ExtendWith;
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.RestAssured;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 
-@ExtendWith({WebHooks.class})
+
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = {"StepDefenition"},
+        glue = {"steps"},
         tags = "@Test",
         plugin = {"pretty", "io.qameta.allure.cucumber6jvm.AllureCucumber6Jvm", "json:target/cucumber.json"}
 )
 
-public class RunnerTest extends WebHooks {
+public class RunnerTest {
+    @BeforeClass
+    public static void before() {
+        RestAssured.filters(new AllureRestAssured());
+    }
+
 
 }
